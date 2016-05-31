b92e7fa27318a90046f262855ccc062a8c24e73c
jonathandavidblack <jonnybl915@gmail.com>
2016-05-31 13:19:46 -0400

    add onAdd and onRemove methods with functionality

---
+++ src/sample/Contact.java
@@ -0,0 +1,24 @@
+package sample;
+
+/**
+ * Created by jonathandavidblack on 5/31/16.
+ */
+public class Contact {
+    String name;
+    String phoneNumber;
+    String email;
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return
+                name +
+                ", phoneNumber'" + phoneNumber +
+                ", email'" + email;
+    }
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,49 @@
 package sample;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionModel;
+import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+
+    @FXML
+    TextField name;
+    @FXML
+    TextField phoneNumber;
+    @FXML
+    TextField email;
+    @FXML
+    ListView list;
+
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    public void onAdd(){
+        Contact contact = new Contact(name.getText(), phoneNumber.getText(), email.getText());
+        if (!(name.getText().isEmpty() || (phoneNumber.getText().isEmpty() || (email.getText().isEmpty())))); {
+        }
+        contacts.add(contact);
+        name.clear();
+    }
+    public void onRemove(){
+        SelectionModel model = list.getSelectionModel();
+        Contact contact = (Contact) model.getSelectedItem();
+        contacts.remove(contact);
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       // if (!(name.getText().isEmpty() || (phoneNumber.getText().isEmpty() || (email.getText().isEmpty())))); {
+        //}
+        list.setItems(contacts);
+
+    }
 }
